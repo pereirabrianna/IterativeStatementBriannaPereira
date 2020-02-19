676860ca4e39eaa4ba19205c6586b3938fbb603a
Brianna Pereira <bpereira2@usf.edu>
2020-02-18 19:41:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IterativeStatementBriannaPereira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Integer between 2 and 10: ");
+
+            int y = Convert.ToInt32(Console.ReadLine());
+
+           if (y % 2 == 0)
+            {
+                for (int i = 1; i < y + 2; i++)
+                {
+                    Console.WriteLine(i.ToString() + "." + "You entered an even number");
+                }
+            }
+            else
+            {
+                for ( int i = 1; i < y + 0; i++)
+                {
+                    Console.WriteLine(i.ToString() + "." + "You entered an odd number");
+                }
+            }
+            }
+
+        }
+    }
+
